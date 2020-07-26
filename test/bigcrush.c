7ec00cb610609894f5edc56d8ee1748ddf42a1d4
Shunsuke Shimizu <grafi@grafi.jp>
2020-07-26 13:03:30 +0900

    update bigcrush

--- test/bigcrush.c
+++ test/bigcrush.c
@@ -1,41 +1,57 @@
-#include <stddef.h>
+#include <stdio.h>
 
-#include <unif01.h>
 #include <bbattery.h>
+#include <unif01.h>
 
-#include <melty2.h>
+#include "melty2.h"
+
+#define BUFLEN 32
+
+static int bitrev;
+
+static melty2_name name;
+static melty2_key key;
+static uint64_t idx = 0;
+
+static uint32_t buf[BUFLEN];
+static int pos = BUFLEN;
 
 static uint32_t next() {
-    static melty2_key key;
-    static uint64_t idx = 0;
-    static uint32_t buf[32];
-    static int pos = 0;
-    if (pos == 0) {
-        melty2_initkey_fromliteral(&key, "melty2", 0, 0);
-        pos = 32;
-    }
-    if (pos == 32) {
+    if (pos == BUFLEN) {
         pos = 0;
-        melty2_bulkgen(&key, idx, 32, buf);
-        idx += 32;
+        melty2_bulkgen(&key, idx, BUFLEN, buf);
+        idx += BUFLEN;
     }
     uint32_t r = buf[pos++];
-#ifdef BITREV
-    uint32_t t;
-    t = r & UINT32_C(0x00FF00FF);
-    r = (t << 16) | (t >> 16) | (t ^ r);
-    t = r & UINT32_C(0x0F0F0F0F);
-    r = (t << 8) | (t >> 24) | (t ^ r);
-    t = r & UINT32_C(0x33333333);
-    r = (t << 4) | (t >> 28) | (t ^ r);
-    t = r & UINT32_C(0x55555555);
-    r = (t << 2) | (t >> 30) | (t ^ r);
-    r = (r << 1) | (r >> 31);
-#endif
+
+    if (bitrev){
+        uint32_t t;
+        t = r & UINT32_C(0x00FF00FF);
+        r = (t << 16) | (t >> 16) | (t ^ r);
+        t = r & UINT32_C(0x0F0F0F0F);
+        r = (t << 8) | (t >> 24) | (t ^ r);
+        t = r & UINT32_C(0x33333333);
+        r = (t << 4) | (t >> 28) | (t ^ r);
+        t = r & UINT32_C(0x55555555);
+        r = (t << 2) | (t >> 30) | (t ^ r);
+        r = (r << 1) | (r >> 31);
+    }
     return r;
 }
 
-int main() {
+int main(int argc, char *argv[]) {
+    if (argc == 1) {
+        bitrev = 0;
+    } else if (argc == 2 && !strcmp(argv[1], "--bitrev")) {
+        bitrev = 1;
+    } else {
+        fputs("usage: bigcrush [--bitrev]", stderr);
+        return 1;
+    }
+
+    melty2_initname(&name, "melty2");
+    melty2_initkey(&key, &name, 0, 0);
+
     unif01_Gen *gen = unif01_CreateExternGenBits("melty2", next);
     bbattery_BigCrush(gen);
     unif01_DeleteExternGenBits(gen);
