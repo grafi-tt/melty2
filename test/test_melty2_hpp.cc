0cb0d6569de7d59e9522a3a446e215abd4e536a5
Shunsuke Shimizu <grafi@grafi.jp>
2020-09-06 10:02:29 +0900

    test ctr

--- test/test_melty2_hpp.cc
+++ test/test_melty2_hpp.cc
@@ -26,7 +26,35 @@ void test_empty_seed() {
     test_assert(result == expected);
 }
 
+void test_ctr() {
+    constexpr size_t N = 123;
+
+    melty2::generator gen;
+    gen.set_ctr(123);
+    std::array<uint32_t, 2 * N> result;
+    for (size_t i = 0; i < N; ++i) {
+        result[i] = gen();
+    }
+    test_assert(gen.current_ctr() == 2 * N);
+    gen.set_ctr(10 * N);
+    for (size_t i = N; i < 2 * N; ++i) {
+        result[i] = gen();
+    }
+    test_assert(gen.current_ctr() == 11 * N);
+
+    melty2_seeder seeder;
+    melty2_initseeder(&seeder);
+    melty2_key key;
+    melty2_initkey(&key, &seeder);
+    std::array<uint32_t, 2 * N> expected;
+    melty2_gen(&key, N, N, expected.data());
+    melty2_gen(&key, 10 * N, N, expected.data() + N);
+
+    test_assert(result == expected);
+}
+
 int main() {
     test_empty_seed();
+    test_ctr();
     return !!err;
 }
