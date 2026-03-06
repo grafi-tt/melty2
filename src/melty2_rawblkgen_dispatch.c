addb082d227c17e6a074eb990beb333cb8b11e20
Shunsuke Shimizu <grafi@grafi.jp>
2026-03-06 17:04:04 +0900

    improve arm dispatch

--- src/melty2_rawblkgen_dispatch.c
+++ src/melty2_rawblkgen_dispatch.c
@@ -4,27 +4,59 @@
 
 #include <cpu_features_macros.h>
 
-#ifdef CPU_FEATURES_ARCH_X86
-#include <cpuinfo_x86.h>
-#define GetInfo() GetX86Info()
-#define Features X86Features
+#ifdef _MSC_VER
+
+#ifdef CPU_FEATURES_ARCH_X86_64
+#define MELTY2_RAWBLKGEN_DISPATCH_X86
 void melty2_rawblkgen_avx512(const melty2_key *key, uint32_t ctr_lo, uint32_t ctr_hi, uint32_t *out);
 void melty2_rawblkgen_avx2(const melty2_key *key, uint32_t ctr_lo, uint32_t ctr_hi, uint32_t *out);
-#ifndef _MSC_VER
-void melty2_rawblkgen_sse2(const melty2_key *key, uint32_t ctr_lo, uint32_t ctr_hi, uint32_t *out);
-#endif
 #endif
 
-#if defined(CPU_FEATURES_ARCH_ARM) && !defined(_MSC_VER)
+#else
+
+#ifdef CPU_FEATURES_ARCH_X86_64
+#define MELTY2_RAWBLKGEN_DISPATCH_X86
+void melty2_rawblkgen_avx512(const melty2_key *key, uint32_t ctr_lo, uint32_t ctr_hi, uint32_t *out);
+void melty2_rawblkgen_avx2(const melty2_key *key, uint32_t ctr_lo, uint32_t ctr_hi, uint32_t *out);
+#endif
+#ifdef CPU_FEATURES_ARCH_X86_32
+#define MELTY2_RAWBLKGEN_DISPATCH_X86
+void melty2_rawblkgen_avx2(const melty2_key *key, uint32_t ctr_lo, uint32_t ctr_hi, uint32_t *out);
+void melty2_rawblkgen_sse2(const melty2_key *key, uint32_t ctr_lo, uint32_t ctr_hi, uint32_t *out);
+#endif
+#ifdef CPU_FEATURES_ARCH_AARCH64
+#define MELTY2_RAWBLKGEN_DISPATCH_AARCH64
+#include <cpuinfo_aarch64.h>
+void melty2_rawblkgen_sve(const melty2_key *key, uint32_t ctr_lo, uint32_t ctr_hi, uint32_t *out);
+#endif
+#ifdef CPU_FEATURES_ARCH_ARM
+#define MELTY2_RAWBLKGEN_DISPATCH_ARM
 #include <cpuinfo_arm.h>
-#define GetInfo() GetArmInfo()
-#define Features ArmFeatures
 void melty2_rawblkgen_neon(const melty2_key *key, uint32_t ctr_lo, uint32_t ctr_hi, uint32_t *out);
+#endif
+
 #endif
 
 void melty2_rawblkgen_generic(const melty2_key *key, uint32_t ctr_lo, uint32_t ctr_hi, uint32_t *out);
 
+#ifdef MELTY2_RAWBLKGEN_DISPATCH_X86
+#include <cpuinfo_x86.h>
+#define Features X86Features
+#define GetInfo() GetX86Info()
+#endif
+#ifdef MELTY2_RAWBLKGEN_DISPATCH_AARCH64
+#include <cpuinfo_aarch64.h>
+#define Features Aarch64Features
+#define GetInfo() GetAarch64Info()
+#endif
+#ifdef MELTY2_RAWBLKGEN_DISPATCH_ARM
+#include <cpuinfo_arm.h>
+#define Features ArmFeatures
+#define GetInfo() GetArmInfo()
+#endif
+
 void melty2_rawblkgen(const melty2_key *key, uint32_t ctr_lo, uint32_t ctr_hi, uint32_t *out) {
+#ifdef GetInfo
     static atomic_int tri_flag = 0;  /* uninitialized: 0, locked: -1, initialized: 1 */
     static Features features;
 
@@ -38,7 +70,7 @@ void melty2_rawblkgen(const melty2_key *key, uint32_t ctr_lo, uint32_t ctr_hi, u
         }
     }
 
-#ifdef CPU_FEATURES_ARCH_X86
+#ifdef CPU_FEATURES_ARCH_X86_64
     if (features.avx512f && features.avx512dq) {
         melty2_rawblkgen_avx512(key, ctr_lo, ctr_hi, out);
         return;
@@ -47,20 +79,29 @@ void melty2_rawblkgen(const melty2_key *key, uint32_t ctr_lo, uint32_t ctr_hi, u
         melty2_rawblkgen_avx2(key, ctr_lo, ctr_hi, out);
         return;
     }
-#ifndef _MSC_VER
+#endif
+#ifdef CPU_FEATURES_ARCH_X86_32
+    if (features.avx2) {
+        melty2_rawblkgen_avx2(key, ctr_lo, ctr_hi, out);
+        return;
+    }
     if (features.sse2) {
         melty2_rawblkgen_sse2(key, ctr_lo, ctr_hi, out);
         return;
     }
 #endif
+#ifdef CPU_FEATURES_ARCH_AARCH64
+    if (features.sve) {
+        melty2_rawblkgen_sve(key, ctr_lo, ctr_hi, out);
+        return;
+    }
 #endif
-
-#if defined(CPU_FEATURES_ARCH_ARM) && !defined(_MSC_VER)
+#ifdef CPU_FEATURES_ARCH_ARM
     if (features.neon) {
         melty2_rawblkgen_neon(key, ctr_lo, ctr_hi, out);
         return;
     }
 #endif
-
+#endif
     melty2_rawblkgen_generic(key, ctr_lo, ctr_hi, out);
 }
