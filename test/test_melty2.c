0f780949b33180282b589252f7196f5d2ed75ee3
Shunsuke Shimizu <grafi@grafi.jp>
2026-03-11 13:28:35 +0900

    update kernel

--- test/test_melty2.c
+++ test/test_melty2.c
@@ -40,14 +40,14 @@ int main(void) {
     melty2_init(&key, 0, 0, 0, 0);
     uint32_t result8[8];
     melty2_gen(&key, 0, 8, result8);
-    n_err += check_number32("ctr=0", result8[0], UINT32_C(0x309af452));
-    n_err += check_number32("ctr=1", result8[1], UINT32_C(0xb985b50b));
-    n_err += check_number32("ctr=2", result8[2], UINT32_C(0x14963309));
-    n_err += check_number32("ctr=3", result8[3], UINT32_C(0x2be93922));
-    n_err += check_number32("ctr=4", result8[4], UINT32_C(0xed2e5e6e));
-    n_err += check_number32("ctr=5", result8[5], UINT32_C(0x6ba46154));
-    n_err += check_number32("ctr=6", result8[6], UINT32_C(0x0e76a8e8));
-    n_err += check_number32("ctr=7", result8[7], UINT32_C(0x50fb7237));
+    n_err += check_number32("ctr=0", result8[0], UINT32_C(0xb374bf06));
+    n_err += check_number32("ctr=1", result8[1], UINT32_C(0xd0f03e79));
+    n_err += check_number32("ctr=2", result8[2], UINT32_C(0x17d9898e));
+    n_err += check_number32("ctr=3", result8[3], UINT32_C(0xf7d3adca));
+    n_err += check_number32("ctr=4", result8[4], UINT32_C(0x6c1c07a1));
+    n_err += check_number32("ctr=5", result8[5], UINT32_C(0x1a540bc4));
+    n_err += check_number32("ctr=6", result8[6], UINT32_C(0x4c97115d));
+    n_err += check_number32("ctr=7", result8[7], UINT32_C(0xfee0ea5a));
 
     for (size_t t = 0; t < sizeof(test_cases) / sizeof(TestCase); ++t) {
         n_err += test_init(&test_cases[t]);
