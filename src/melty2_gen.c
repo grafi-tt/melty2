4d080394c1d0647b9e067144754af19e2683ab08
Shunsuke Shimizu <grafi@grafi.jp>
2026-04-02 09:47:50 +0900

    simplify melty2_gen loop

--- src/melty2_gen.c
+++ src/melty2_gen.c
@@ -17,13 +17,12 @@ void melty2_gen(const melty2_key *key, uint64_t ctr, size_t len, uint32_t *out) 
     for (;;) {
         for (size_t k = 0; k < peel_len; ++k) {
             uint32_t v[6] = {key_v[0], key_v[1], key_v[2], key_v[3], key_v[4], key_v[5]};
-            uint64_t ctr_k = ctr + (uint64_t)k;
-            out_p[k] = melty2_kernel(v, (uint32_t)ctr_k, (uint32_t)(ctr_k >> 32));
+            out_p[k] = melty2_kernel(v, (uint32_t)ctr, (uint32_t)(ctr >> 32));
+            ++ctr;
         }
         if ((len -= peel_len) == 0) {
             return;
         }
-        ctr += peel_len;
         out_p += peel_len;
 
         while (len >= MELTY2_RAWBLKLEN) {
